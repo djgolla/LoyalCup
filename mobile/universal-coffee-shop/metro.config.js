4cfe2479a324b22a91e81ea74823665b8ba34b03
djgolla <golladaydaniel@gmail.com>
2026-06-12 10:50:59 -0500

    V1.0 App Store Builds

--- mobile/universal-coffee-shop/metro.config.js
+++ mobile/universal-coffee-shop/metro.config.js
@@ -3,8 +3,6 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// This allows Metro to resolve modules from your project's root
-config.resolver.unstable_enableSymlinks = true;
 config.resolver.unstable_enablePackageExports = true;
 
 module.exports = config;
