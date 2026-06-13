25af237087a3b653d1f4d05a3b4294c04c44c89d
djgolla <golladaydaniel@gmail.com>
2026-06-13 11:05:52 -0500

    fix packages for production

--- mobile/universal-coffee-shop/metro.config.js
+++ mobile/universal-coffee-shop/metro.config.js
@@ -3,6 +3,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-config.resolver.unstable_enablePackageExports = true;
-
 module.exports = config;
